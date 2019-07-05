ba58b5a548a88dfa272417214d6bbd5a887ec921
Julia <julcia106@onet.eu>
2019-07-05 10:51:47 +0200

    Add project files.

---
+++ RobertLafore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RobertLafore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
