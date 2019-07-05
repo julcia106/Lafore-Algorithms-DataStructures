660b4198ce40ff19e54133f194dfe69bf7f727cf
Julia <julcia106@onet.eu>
2019-07-05 12:06:22 +0200

    Array

---
+++ Rozdział 2/1/HighArrayApp.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1
+{
+    class HighArrayApp
+    {
+        static void Main(string []args)
+        {
+            int maxSize = 100; //array size
+            HighArray arr;     //reference to array
+            arr = new HighArray(maxSize); //create the array
+
+            arr.insert(77);
+            arr.insert(22);
+            arr.insert(55);
+            arr.insert(1);
+            arr.insert(54);
+            arr.insert(3);
+            arr.insert(23);
+            arr.insert(76);
+            arr.insert(65);
+            arr.insert(7);
+
+            arr.display();
+
+            int searchKey = 35; //search for item
+            if (arr.find(searchKey))
+                Console.WriteLine("Found " + searchKey);
+            else
+                Console.WriteLine("Can't find " + searchKey);
+
+            arr.delete(3);
+            arr.delete(23);
+            arr.delete(7);
+
+            arr.display();
+        }
+    }
+}
