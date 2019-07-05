dc5222659161af875fec2f46b574d58bacc517ba
Julia <julcia106@onet.eu>
2019-07-05 11:12:39 +0200

    .

--- Rozdział 2/2/Program.cs
+++ Rozdział 2/2/Program.cs
@@ -6,7 +6,7 @@ namespace _2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Worrrld!");
         }
     }
 }
