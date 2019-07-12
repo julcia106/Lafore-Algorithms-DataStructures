4ec0c68ebe31b849502480c7c5058d00507cfe45
Julia <julcia106@onet.eu>
2019-07-12 17:57:16 +0200

    BinarySearch

--- Rozdział 2/BinarySearch/BinarySearch.cs
+++ Rozdział 2/BinarySearch/BinarySearch.cs
@@ -19,11 +19,12 @@ namespace BinarySearch
             {
                 Console.Write(a[i]+ " ");
             }
+            Console.WriteLine("");
         }
 
         public int size() => nElems;
 
-        public int find(long searchKey)
+        public int iterative_find(long searchKey)
         {
             int right = nElems - 1;
             int left = 0;
@@ -44,6 +45,23 @@ namespace BinarySearch
                         right = curIn - 1;
                 }
             }
+        }
+
+        public int recursive_find(long searchKey,int left,int right)
+        {
+            if (left > right)
+                return -1;
+
+            int curIn = (left + right) / 2;
+            if (a[curIn] == searchKey)
+                return curIn;
+
+            else if (searchKey < a[curIn])   //jest w górnej połowie
+            {
+                return recursive_find(searchKey, left, curIn - 1);
+            }
+            else
+                return recursive_find(searchKey, curIn - 1, right);
         }
 
         public void insert(long value)
@@ -81,15 +99,14 @@ namespace BinarySearch
 
         public void delete(long value)
         {
-            for (int i = 0; i < nElems; i++) 
+            for(int i=0; i<nElems; i++)
             {
                 if (a[i] == value)
                 {
-                    a[i] = a[i + 1];
+                    for (int j = i; j < nElems; j++)
+                        a[j] = a[j + 1];
                     nElems--;
                 }
-                else
-                    Console.WriteLine("There is no " + value + " in the array.");
             }
         }
     }
@@ -103,21 +120,27 @@ namespace BinarySearch
             Random rand = new Random();
             Array arr = new Array(maxSize);
 
-            //for (int i = 0; i < maxSize; i++)
+            //for (int i = 0; i < maxSize; i++) 
             //{
             //    long n = (long)(rand.Next(10));
             //    arr.insert(n);
             //}
 
             arr.insert(1);
-            arr.insert(4);
+            arr.insert(2);
+            arr.insert(8);
             arr.insert(12);
-            arr.insert(14);
+            arr.insert(22);
+            arr.insert(55);
+            arr.insert(56);
+            arr.insert(59);
+            arr.insert(60);
+            arr.insert(66);
 
             arr.display();
-            arr.delete(4);
-            arr.display();
-            
+
+            Console.WriteLine("Index of your searchKey: " + arr.recursive_find(2, 0, 9));
+            Console.WriteLine("Index of your searchKey: " + arr.iterative_find(59));
         }
     }
 }
