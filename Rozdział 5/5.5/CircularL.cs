61915cfad553f3846d65c8a8acaf5fb92144bf29
Julia <julcia106@onet.eu>
2019-08-01 17:30:13 +0200

    JosephusProblem

---
+++ Rozdział 5/5.5/CircularL.cs
@@ -0,0 +1,148 @@
+﻿//5.5 The Josephus Problem is a famous mathematical puzzle that goes back to
+//ancient times.There are many stories to go with the puzzle. One is that
+//Josephus was one of a group of Jews who were about to be captured by the
+//Romans. Rather than be enslaved, they chose to commit suicide.They arranged
+//themselves in a circle and, starting at a certain person, started counting off
+//around the circle. Every nth person had to leave the circle and commit suicide.
+//Josephus decided he didn’t want to die, so he arranged the rules so he would
+//be the last person left.If there were (say) 20 people, and he was the seventh
+//person from the start of the circle, what number should he tell them to use for
+//counting off? The problem is made much more complicated because the circle
+//shrinks as the counting continues.
+//Create an application that uses a circular linked list (like that in Programming
+//Project 5.3) to model this problem.Inputs are the number of people in the
+//circle, the number used for counting off, and the number of the person where
+//248 CHAPTER 5 Linked Lists
+//counting starts (usually 1). The output is the list of persons being eliminated.
+//When a person drops out of the circle, counting starts again from the person
+//who was on his left (assuming you go around clockwise). Here’s an example.
+//There are seven people numbered 1 through 7, and you start at 1 and count off
+//by threes. People will be eliminated in the order 4, 1, 6, 5, 7, 3. Number 2 will
+//be left.
+
+using System;
+
+namespace containter
+{
+    class Link
+    {
+        public int data;
+        public Link next;
+
+        public Link(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+
+    class CircularList
+    {
+        private Link curr;
+
+        public CircularList()
+        {
+            curr = null;
+        }
+
+        public void insert(int value)
+        {
+            Link newLink = new Link(value);
+
+            if(isEmpty())
+            {
+                newLink.next = newLink;
+                curr = newLink;
+            }
+            else
+            {
+                newLink.next = curr.next;
+                curr.next = newLink;
+                curr = newLink;
+            }
+        }
+
+        public void delete()
+        {
+            Link temp = curr;
+
+            if(temp.next== curr)
+            {
+                curr = null;
+            }
+            else
+            {
+                do
+                {
+                    temp = temp.next;
+                }
+                while (temp.next != curr);
+                {
+                    temp.next = curr.next;
+                    curr = temp;
+                }
+            }
+        }
+
+        public void display()
+        {
+            Link temp = curr;
+
+            if(curr!= null)
+            {
+                do
+                {
+                    temp = temp.next;
+                    Console.Write(temp.data + " ");
+                }
+                while (temp != curr);
+                {
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("List is empty.");
+            }
+        }
+
+        public void move()
+        {
+            if(!isEmpty())
+            {
+                curr = curr.next;
+            }
+        }
+
+        public bool isEmpty()
+        {
+            return curr == null;
+        }
+    }
+
+    class CircularListApp
+    {
+        static void Main(String[] args)
+        {
+            const int People = 20;
+            const int Count = 5;
+
+            CircularList one = new CircularList();
+
+            for(int i=1; i<= People; i++)
+            {
+                one.insert(i);
+            }
+
+            while(!one.isEmpty())
+            {
+                one.display();
+                for(int i=0; i< Count; i++)
+                {
+                    one.move();
+                }
+                one.delete();
+            }
+        }
+    }
+}
---
+++ Rozdział 5/iterator/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+
+namespace conatiner
+{
+    class Link
+    {
+        public int data;
+        public Link next;
+
+        public Link(int value)
+        {
+            data = value;
+            next = null;
+        }
+
+        public void displayNode()
+        {
+            Console.Write(data + " ");
+        }
+    }
+    //--------------------------------------------------------------------------
+    class ListIterator
+    {
+        private Link current;
+        private Link previous;
+        private LinkList ourList;           //lista, na której operuje iterator
+
+        public ListIterator(LinkList list)  //konstruktor
+        {
+            ourList = list;
+            reset();
+        }
+
+        public void reset()                 //powrót na początek listy
+        {
+            current = ourList.getFirst();
+            previous = null;
+        }
+
+        public bool atEnd() => current.next == null;
+
+        public void nextLink()
+        {
+            previous = current;
+            current = current.next;
+        }
+
+        public Link getCurrent() => current;
+
+        public void insertAfter(int data)
+        {
+            Link newLink = new Link(data);
+
+            if (ourList.isEmpty())
+            {
+                ourList.setFirst(newLink);
+                current = newLink;
+            }
+            else
+            {
+                newLink.next = current.next;
+                current.next = newLink;
+                nextLink();                     //przechodzimy do nowo wstawionego elementu
+            }
+        }
+
+        public void insertBefore(int data)
+        {
+            Link newLink = new Link(data);
+
+            if (previous == null)
+            {
+                newLink.next = ourList.getFirst();
+                ourList.setFirst(newLink);
+                reset();
+            }
+            else
+            {
+                newLink.next = previous.next;
+                previous.next = newLink;
+                current = newLink;
+            }
+        }
+
+        public int deleteCurrent()
+        {
+            int value = current.data;
+
+            if (previous == null)
+            {
+                ourList.setFirst(current.next);
+                reset();
+            }
+            else
+            {
+                previous.next = current.next;
+                if (atEnd())
+                    reset();
+                else
+                    current = current.next;
+            }
+            return value;
+        }
+    }
+    //-------------------------------------------------------------------------
+    class LinkList
+    {
+        private Link first;
+
+        public LinkList()
+        {
+            first = null;
+        }
+
+        public Link getFirst() => first;
+
+        public void setFirst(Link f)                //ustawiamy referencję do pierwszego elementu
+        {
+            first = f;
+        }
+
+        public bool isEmpty() => first == null;
+
+        public ListIterator getIterator()           //zwraca iterator
+        {
+            return new ListIterator(this);          //zainicjowany danymi (this)tej listy
+        }
+
+        public void displayList()
+        {
+            Link current = first;
+            while (current != null)
+            {
+                current.displayNode();
+                current = current.next;
+            }
+            Console.WriteLine();
+        }
+    }
+    //--------------------------------------------------------------------------
+    class CircularListApp
+    {
+        static void Main(string[] args)
+        {
+            LinkList one = new LinkList();              //new list
+
+            ListIterator iter1 = one.getIterator();     //new iterator
+
+            for (int i=1; i<= 20; i++)
+            {
+                iter1.insertAfter(i);
+            }
+
+            one.displayList();
+
+            iter1.reset();
+            iter1.deleteCurrent();
+            while (!iter1.atEnd())
+            {
+                while (!iter1.atEnd())
+                {
+                    iter1.nextLink();
+                    iter1.deleteCurrent();
+                }
+                iter1.reset();
+            }
+
+            one.displayList();
+        }
+    }
+}
