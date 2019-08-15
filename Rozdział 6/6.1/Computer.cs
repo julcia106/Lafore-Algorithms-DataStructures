86a0c5f3cd8da157cbd77a0fd0ec428535607dd6
Julia <julcia106@onet.eu>
2019-08-15 09:39:36 +0200

    recursion

--- Rozdział 6/6.1/Computer.cs
+++ Rozdział 6/6.1/Computer.cs
@@ -4,6 +4,9 @@
 //of x and y by adding x to itself y times. Its arguments are x and y and its return
 //value is the product of x and y. Write such a method and a main() program to
 //call it.
+//6.3 Implement the recursive approach to raising a number to a power, as described
+//in the “Raising a Number to a Power” section near the end of this chapter.
+//Write the recursive power() function and a main() routine to test it.
 
 using System;
 
@@ -11,7 +14,7 @@ namespace Recursion
 {
     class Computer
     {
-        public int mult(int y, int x)
+        public int Mult(int y, int x)
         {
             if (x == 0) 
             {
@@ -19,13 +22,32 @@ namespace Recursion
             }
             else
             {
-                return (y + mult(y, x - 1));
+                return (y + Mult(y, x - 1));
             }
         }
+
+        public int NumToPower(int x, int y)
+        {
+            if (y == 1)         // end 
+            {
+                return x;
+            }
+            else if (y % 2 == 1)   // y is odd number
+            {
+                return x * NumToPower(x, y - 1);
+            }
+            else
+            {
+                return NumToPower(x * x, y / 2);
+            }
+            
+        }
+
         static void Main(string[] args)
         {
             Computer one = new Computer();
-            Console.WriteLine(one.mult(2,3));
+            Console.WriteLine(one.Mult(2,3));
+            Console.WriteLine(one.NumToPower(2, 5));
         }
     }
 }
